8bc0179553fb1b90a87f29420e7c620e81092362
kyungmiiiii <kyungmiiiii52@gmail.com>
2025-08-14 18:20:41 +0900

    Update CommonKey.java
    API 수정

--- Wayble/src/main/java/com/wayble/util/CommonKey.java
+++ Wayble/src/main/java/com/wayble/util/CommonKey.java
@@ -1,5 +1,5 @@
 package com.wayble.util;
 
 public class CommonKey {
-	public static final String googleKey = "AIzaSyBO292cTbolXbrl7Q_mRicCmyHObBb-AUA";
+	public static final String googleKey = "API KEY 입력";
 }
